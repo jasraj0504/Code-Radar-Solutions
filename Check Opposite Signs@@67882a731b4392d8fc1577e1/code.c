b5684c8b21658a72afe41219b4e162228e8eda3e
jasraj0504 <jasraj0504.becse24@chitkara.edu.in>
2025-02-16 18:14:51 +0530

    Solution to Check Opposite Signs

--- Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
+++ Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int a = 0, b = 0; // Initialize the variables
-    
-    scanf("%d %d", &a, &b); // Take input from the user
+    int a = 7, b = 3; // Initialize the variables
 
-    if(a > 0)
-        printf("True\n"); // Print True if a is greater than 0
+    if(a > 0 && b > 0)
+        printf("True\n"); // Print True if both a and b are greater than 0
     else
         printf("False\n"); // Print False otherwise
 
