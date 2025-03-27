9fbe6228961abef434e89d1c0d070c853c75c61a
jasraj0504 <jasraj0504.becse24@chitkara.edu.in>
2025-03-27 15:35:34 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,16 +1,30 @@
-#include<stdio.h>
-int main(){
-int a,b;
-char c;
-scanf("%d",&a);
-scanf("%d",&b);
-scanf("%c",&c);
-if(c=='+')
-   {printf("%d",a+b);}
-   else if(c=='-'){printf("%d",a-b);}
-   else if(c=='*'){printf("%d",a*b);}
-   else{printf("%d",a/b);}
-   return 0;
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    char c;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf(" %c", &c);  // Note the space before %c to ignore newline
+
+    if (c == '+') {
+        printf("%d", a + b);
+    } else if (c == '-') {
+        printf("%d", a - b);
+    } else if (c == '*') {
+        printf("%d", a * b);
+    } else if (c == '/') {
+        if (b == 0) {
+            printf("Error: Division by zero");
+        } else {
+            printf("%d", a / b);
+        }
+    } else {
+        printf("Invalid operator");
+    }
+
+    return 0;
 }
 
 
